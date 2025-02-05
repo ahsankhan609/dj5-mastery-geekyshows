d99c4168ddbd16991268129b4801ba1fd42a4e6d
ahsankhan609 <ahsankhan609@gmail.com>
2025-02-06 00:11:18 +0500

    update settings

--- main/settings.py
+++ main/settings.py
@@ -25,6 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+# If no environment variable is set, generate a new key
+if not SECRET_KEY:
+    from django.core.management.utils import get_random_secret_key
+    SECRET_KEY = get_random_secret_key()
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
